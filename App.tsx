b1abf2e7519124a7a237f052ec2dd6dfc179e0e1
Kawsar Hossain <thekawsarhossain@gmail.com>
2024-01-17 17:27:44 +0600

    one signal initial set

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/Login';
 import Notice from './src/screens/Notice';
 import useAuth from './src/hooks/useAuth';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, AppState } from 'react-native';
+import { LogLevel, OneSignal } from "react-native-onesignal";
+import { useEffect } from 'react';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,8 +19,27 @@ const THEME = {
 };
 
 export default function App() {
-
   const { isLoggedin, loading } = useAuth();
+
+  const initOneSignal = async () => {
+    OneSignal.initialize(process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID as string);
+    OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+    // OneSignal.login("hello_world");
+
+    const hasPermission = OneSignal.Notifications.hasPermission()
+    console.log("Device State: ", hasPermission)
+    if (!hasPermission) await OneSignal.Notifications.requestPermission(true);
+  };
+
+  useEffect(() => {
+    initOneSignal();
+
+    AppState.addEventListener('change', (nextAppState) => {
+      if (nextAppState === 'active') {
+        initOneSignal();
+      }
+    });
+  }, []);
 
   if (loading) return <ActivityIndicator className='flex items-center justify-center h-screen' />
 
